3f82bc898cf75716d6ef1e5861007e66d590df32
귀챠니즘 <yjm0003@naver.com>
2022-11-17 11:16:05 +0900

    Update clip_downloader.py

--- clip_downloader.py
+++ clip_downloader.py
@@ -7,13 +7,25 @@ if __name__ == "__main__":
     if file_extend == 'json':
         with open(file_path, 'r', encoding='UTF-8') as file:
             data = json.load(file)
-        clip_link = list(map(lambda x:x['url'], data))
+        clip_link = list(map(lambda x:[x['url'],x['title'],x['created_at'],x['gamename']], data))
+        for item in clip_link:
+            time = item[2].split("T")[0]
+            title = item[1]
+            game = item[3]
+            print(item[0])
+            ydl_opts = {
+                'outtmpl': f'({time} {game}) {title}.mp4',
+                'format': 'bestvideo/best'
+                }
+            print(ydl_opts)
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([item[0]])
     elif file_extend == 'txt':
         with open(file_path, 'r', encoding='UTF-8') as file:
             data = file.read()
         clip_link = data.split("\n")
-    ydl_opts = {
-        'format': 'bestvideo/best'
-        }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(clip_link)
+        ydl_opts = {
+            'format': 'bestvideo/best'
+            }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(clip_link)
