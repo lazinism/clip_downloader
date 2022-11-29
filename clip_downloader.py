cfa53271e026c06bc6ab9b3eb570065974b8365b
귀챠니즘 <yjm0003@naver.com>
2022-11-29 15:40:14 +0900

    Escape 방식 변경, 실패 시 재시도(5회)
    #1

--- clip_downloader.py
+++ clip_downloader.py
@@ -1,5 +1,9 @@
 import json
 import youtube_dl
+from functools import reduce
+
+forbidden_chars = "%:/\\<>*?？|！\n"
+max_try = 5
 
 if __name__ == "__main__":
     file_path = input("파일 이름 입력: ")
@@ -10,14 +14,21 @@ if __name__ == "__main__":
         clip_link = list(map(lambda x:[x['url'],x['title'],x['created_at'],x['gamename']], data))
         for item in clip_link:
             time = item[2].split("T")[0]
-            title = item[1].replace("%", "").replace("/","_").replace("\\","").replace("?","").replace("*", "").replace("<","").replace(">","").replace("|","").replace(":","-")
-            game = item[3].replace("%", "").replace("/","_").replace("\\","").replace("?","").replace("*", "").replace("<","").replace(">","").replace("|","").replace(":","-")
-            ydl_opts = {
-                'outtmpl': f'({time} {game}) {title}.mp4',
-                'format': 'bestvideo/best'
-                }
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([item[0]])
+            title = reduce(lambda x,y: x.replace(y, "_"), list(forbidden_chars), item[1])
+            game = reduce(lambda x,y: x.replace(y, "_"), list(forbidden_chars), item[3])
+            err = 0
+            while err < max_try:
+                try:
+                    ydl_opts = {
+                        'outtmpl': f'({time} {game}) {title}.mp4',
+                        'format': 'bestvideo/best'
+                    }
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([item[0]])
+                except:
+                    err += 1
+                else:
+                    err = 0    
     elif file_extend == 'txt':
         with open(file_path, 'r', encoding='UTF-8') as file:
             data = file.read()
